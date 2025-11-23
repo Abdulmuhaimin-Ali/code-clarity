63b612ba8210a76ac3f6a87044ecd6f8a8efa838
Abdul <abdulali115h@gmail.com>
2025-11-23 05:15:31 -0500

    commented demo code

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import webhook from "./src/webhook.js";
-import userRoutes from "./src/routes/userRoutes.js";
-import postRoutes from "./src/routes/postRoutes.js";
-import sessionRoutes from "./src/routes/sessionRoutes.js";
+// import userRoutes from "./src/routes/userRoutes.js";
+// import postRoutes from "./src/routes/postRoutes.js";
+// import sessionRoutes from "./src/routes/sessionRoutes.js";
 
 dotenv.config();
 
@@ -14,10 +14,10 @@ app.use(bodyParser.json({ limit: "10mb" }));
 // GitHub webhook endpoint
 app.post("/webhook", webhook);
 
-// API endpoints for Prisma models
-app.use("/users", userRoutes);
-app.use("/posts", postRoutes);
-app.use("/sessions", sessionRoutes);
+// // API endpoints for Prisma models
+// app.use("/users", userRoutes);
+// app.use("/posts", postRoutes);
+// app.use("/sessions", sessionRoutes);
 
 const PORT = process.env.PORT || 3000;
 
